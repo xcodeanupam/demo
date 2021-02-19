68246276104425a500e2c7264941a9d86b1d5e44
Anupam <xanukum@gmail.com>
2021-02-19 15:33:44 +0530

    s

--- src/app/layout/blog-three/blog-three.component.ts
+++ src/app/layout/blog-three/blog-three.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
   selector: 'app-blog-three',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogThreeComponent implements OnInit {
 
-  constructor() { }
+  title: string | null | undefined;
+  blog: any;
+
+  constructor(public userService: UserService,  public router: Router, public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.title = params.get('title');
+    });
+    console.log('category name', this.title)
+    this.getAblog();
+  }
+
+  getAblog() {
+    this.userService.getBlog(this.title).subscribe(
+      (data: any) => {
+        this.blog = data[0];
+        console.log('blog data', data)
+      },
+      error => {
+        console.log('error', error);
+      }
+    );
   }
 
 }
