50f0e8b9b7bb5595ca087a41e8c6ce1052bdc794
Anupam <xanukum@gmail.com>
2021-03-02 10:26:35 +0530

    s

--- server/models/category.js
+++ server/models/category.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 var CategorySchema = new mongoose.Schema({
     categoryname: { type: String, lowercase: true, trim: true },
     category_id: { type: String},
+    categorylink: { type: String },
     description: { type: String },
-    total: { type: String },
+    image: { type: String },
 }, { timestamps: true });
 
 var Category = mongoose.model('categorys', CategorySchema);
